03fefec8833416f461ecbeecc8523de666dec295
Sean Nemann <nemann.3@buckeyemail.osu.edu>
2019-04-02 23:31:09 -0400

    Created structure of website and title page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,126 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Footer extends React.Component {
+	
+	render() {
+		return (
+			<div>
+				<h6>
+					footer
+				</h6>
+			</div>
+		);
+	}
+}
+
+class Music extends React.Component {
+	render() {
+		return (
+			<div className="container-fluid full-page music-page">
+				<div className="row">
+					<h1 className="col page-title-text">
+						Description
+					</h1>
+				</div>
+			</div>
+		);
+	}
+}
+
+class Project extends React.Component {
+
+}
+
+class Summary extends React.Component {
+	
+	render() {
+		return (
+			<div className="container-fluid full-page description-page">
+				<div className="row">
+					<h1 className="col page-title-text">
+						Description
+					</h1>
+				</div>
+			</div>
+		);
+	}
+}
+
+class Greeter {
+	constructor() {
+		this.hellos = [
+			"Hello",
+			"Howdy",
+			"Hi",
+			"Hey"
+		]
+
+		this.worlds = [
+			"World",
+			"Planet",
+			"Earth",
+			"Universe"
+		]
+	}
+	
+
+	getGreeting() {
+		return this.hellos[Math.floor(Math.random() * this.hellos.length)] + ', ' + this.worlds[Math.floor(Math.random() * this.worlds.length)];
+	}
+}
+
+class TitlePage extends React.Component {
+
+	constructor(props) {
+		super(props);
+		const greeter = new Greeter();
+		this.state = {title: greeter.getGreeting()}
+	}
+
+	render() {
+		return (
+			<div className="container-fluid full-page title-page">
+				<div className="row">
+					<div className="col main-title">
+						<div className="main-title-text">
+							{this.state.title}
+						</div>
+						<div className="main-sub-title-text">
+							An Introduction to Me (Sean Nemann)
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+class Main extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {projects: []};
+	}
+
+	render() {
+		return (
+			<>
+			<TitlePage/>
+			<Summary/>
+				{this.state.projects.map( project => <Project title={project.title} demoSource={project.demoSource} description={project.description} 
+																primaryColor={project.primaryColor} secondaryColor={project.secondaryColor}/>)}
+			<Music/>
+			<Footer/>
+			</>
+		);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
